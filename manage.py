f4950d11b591f309f40413ddeb59ee97596892bf
lixiaoyao <liyan__mail@163.com>
2018-10-14 19:58:28 +0800

    session保存位置指定以及测试

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
+# 可以用来指定Session保存的位置
+from flask_session import Session
 
 
 class Config(object):
     """项目的配置"""
     DEBUG = True
+    SECRET_KEY = "7S04rmpWCcwlK4zPwDH1hScf876FASCAqXpxmLj/E8hoaUQpcCEJ1M0bbCBlClF1"
 
     # 为数据库添加配置
     SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:root@127.0.0.1:3306/information"
@@ -15,6 +18,18 @@ class Config(object):
     # redis的配置
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+    REDIS_PASSWORD = "root"
+
+    # session保存配置
+    SESSION_TYPE = "redis"
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 指代session 保存的 redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
 app = Flask(__name__)
@@ -24,16 +39,17 @@ app.config.from_object(Config)
 
 # 初始化数据库
 db = SQLAlchemy(app)
-
 # 初始化redis存储对象
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-
 # 开启当前项目CSRF保护
 CSRFProtect(app)
+# 设置session保存位置
+Session(app)
 
 
 @app.route('/')
 def index():
+    session['name'] = "zhangsan"
     return 'Hello World'
 
 
