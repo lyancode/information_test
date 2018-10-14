6df22eab833c01b649d87f272365f82c411bdc66
lixiaoyao <liyan__mail@163.com>
2018-10-14 21:36:32 +0800

    抽取app创建到函数中&配置名字作为参数

--- info/__init__.py
+++ info/__init__.py
@@ -6,16 +6,21 @@ from redis import StrictRedis
 from flask_session import Session
 from config import config
 
-app = Flask(__name__)
-
-# 加载配置
-app.config.from_object(config["development"])
-
 # 初始化数据库
-db = SQLAlchemy(app)
-# 初始化redis存储对象
-redis_store = StrictRedis(host=config["development"].REDIS_HOST, port=config["development"].REDIS_PORT)
-# 开启当前项目CSRF保护
-CSRFProtect(app)
-# 设置session保存位置
-Session(app)
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # 加载配置
+    app.config.from_object(config[config_name])
+    # 通过app初始化
+    db.init_app(app)
+    # 初始化redis存储对象
+    redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT)
+    # 开启当前项目CSRF保护
+    CSRFProtect(app)
+    # 设置session保存位置
+    Session(app)
+
+    return app
