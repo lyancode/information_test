2dc8122e6c950dc4d4a8ac1d63651b2df989419b
lixiaoyao <2802277269@qq.com>
2018-10-16 19:46:03 +0800

    前端加载更多数据的实现

--- info/static/news/js/index.js
+++ info/static/news/js/index.js
@@ -43,7 +43,18 @@ $(function () {
         var nowScroll = $(document).scrollTop();
 
         if ((canScrollHeight - nowScroll) < 100) {
-            // TODO 判断页数，去更新新闻数据
+            // 判断页数，去更新新闻数据
+            if (!data_querying) {
+            // 将`是否正在向后端查询新闻数据`的标志设置为真
+                data_querying = true;
+                // 如果当前页面数还没到达总页数
+                if(cur_page < total_page) {
+                    cur_page +=1;
+                    // 向后端发送请求，查询下一页新闻数据
+                    updateNewsData();
+                }
+            }
+
         }
     })
 })
@@ -55,25 +66,32 @@ function updateNewsData() {
         "page": cur_page
     }
     $.get("/news_list", params, function (resp) {
-        if (resp.errno == "0") {
-            // 代表请求成功
-            // 清除已有数据
-            $(".list_con").html("");
+            // 数据加载完毕，设置【正在加载数据】的变量为false，代表当前没有数据在加载
+            data_querying = false
 
-            for (var i = 0; i < resp.data.news_dict_li.length; i++) {
-                var news = resp.data.news_dict_li[i]
-                var content = '<li>'
-                content += '<a href="#" class="news_pic fl"><img src="' + news.index_image_url + '?imageView2/1/w/170/h/170"></a>'
-                content += '<a href="#" class="news_title fl">' + news.title + '</a>'
-                content += '<a href="#" class="news_detail fl">' + news.digest + '</a>'
-                content += '<div class="author_info fl">'
-                content += '<div class="source fl">来源：' + news.source + '</div>'
-                content += '<div class="time fl">' + news.create_time + '</div>'
-                content += '</div>'
-                content += '</li>'
-                $(".list_con").append(content)
-            }
-        }else{
+            if (resp.errno == "0") {
+                // 给总页数赋值
+                total_page = resp.data.total_page;
+                // 代表请求成功
+                // 清除已有数据
+                if (cur_page == "1"){
+                    $(".list_con").html("");
+                }
+
+                for (var i = 0; i < resp.data.news_dict_li.length; i++) {
+                    var news = resp.data.news_dict_li[i]
+                    var content = '<li>'
+                    content += '<a href="#" class="news_pic fl"><img src="' + news.index_image_url + '?imageView2/1/w/170/h/170"></a>'
+                    content += '<a href="#" class="news_title fl">' + news.title + '</a>'
+                    content += '<a href="#" class="news_detail fl">' + news.digest + '</a>'
+                    content += '<div class="author_info fl">'
+                    content += '<div class="source fl">来源：' + news.source + '</div>'
+                    content += '<div class="time fl">' + news.create_time + '</div>'
+                    content += '</div>'
+                    content += '</li>'
+                    $(".list_con").append(content)
+                }
+            } else {
                 // 请求失败
                 alert(resp.errmsg)
             }
