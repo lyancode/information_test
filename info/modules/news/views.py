008c07ab1b24f1355ad3510295bace9ccc39d0c7
lixiaoyao <liyan__mail@163.com>
2018-10-18 19:17:01 +0800

    显示当前新闻的评论数据

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -160,17 +160,32 @@ def news_detail(news_id):
     # 更新新闻的点击次数
     news.clicks += 1
 
+    # 是否是收藏
     is_collected = False
     if user:
         # 判断用户是否收藏当前新闻
         if news in user.collection_news:
             is_collected = True
 
+    # 去查询评论数据
+    comments = []
+    try:
+        comments = Comment.query.filter(Comment.news_id == news_id).order_by(Comment.create_time.desc()).all()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    comment_dict_li = []
+
+    for comment in comments:
+        comment_dict = comment.to_dict()
+        comment_dict_li.append(comment_dict)
+
     data = {
         "user": user.to_dict() if user else None,
         "news_dict_li": news_dict_li,
         "news": news.to_dict(),
-        "is_collected": is_collected
+        "is_collected": is_collected,
+        "comments":comment_dict_li
     }
 
     return render_template('news/detail.html', data=data)
