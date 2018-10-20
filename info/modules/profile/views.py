507c42fc6ec2b0331ecb3e534dabdfa7e6717f76
lixiaoyao <liyan__mail@163.com>
2018-10-20 19:26:30 +0800

    用户发布新闻的前后端实现

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -9,10 +9,40 @@ from info.utils.image_storage import storage
 from info.utils.response_code import RET
 
 
+@profile_blue.route('/news_release')
+@user_login_data
+def user_news_list():
+    page = request.args.get("p", 1)
+    user = g.user
+
+    news_list = []
+    current_page = 1
+    total_page = 1
+    try:
+        paginate = News.query.filter(News.user_id == user.id).paginate(page, constants.USER_COLLECTION_MAX_NEWS, False)
+        news_list = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_dict_li = []
+    for news in news_list:
+        news_dict_li.append(news.to_review_dict())
+
+    data = {
+        "news_list": news_dict_li,
+        "current_page": current_page,
+        "total_page": total_page
+
+    }
+
+    return render_template('news/user_news_list.html', data=data)
+
+
 @profile_blue.route('/news_release', methods=['POST', 'GET'])
 @user_login_data
 def news_release():
-
     if request.method == 'GET':
         # 加载新闻分类数据
         categories = []
