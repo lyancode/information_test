53e8d0dff9a1bfd844b519f706892203b4dc0215
lixiaoyao <liyan__mail@163.com>
2018-10-20 15:44:22 +0800

    基本资料修改

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,8 +1,9 @@
-from flask import render_template, g, redirect, request
+from flask import render_template, g, redirect, request, jsonify
 from sqlalchemy.sql.functions import user
 
 from info.modules.profile import profile_blue
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 
 
 @profile_blue.route('/base_info', methods=['POST', 'GET'])
@@ -11,6 +12,25 @@ def base_info():
     # 不同的请求方式，做不同的事情
     if request.method == "GET":
         return render_template('news/user_base_info.html', data={"user": g.user.to_dict()})
+
+    # 代表修改用户数据
+    # 1、取到传入的参数
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
+
+    # 2、检验参数
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    if gender not in ["MAN", "WOMAN"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    user = g.user
+    user.nick_name = nick_name
+    user.signature = signature
+    user.gender = gender
+
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 
 @profile_blue.route('/info')
