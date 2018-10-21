9e7ef820b58462dcfee01e16e6b054e4d6febb3d
lixiaoyao <liyan__mail@163.com>
2018-10-21 18:55:32 +0800

    总人数&月新增数&日新增数

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,9 +1,48 @@
+import time
+from datetime import datetime
+
 from flask import render_template, request, jsonify, current_app, session, redirect, url_for, g
 
 from info.models import User
 from info.modules.admin import admin_blue
 from info.utils.common import user_login_data
 from info.utils.response_code import RET
+
+
+@admin_blue.route('user_count')
+def user_count():
+    # 总人数
+    total_count = 0
+    try:
+        total_count = User.query.filter(User.is_admin == False).count()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 月新增数
+    mon_count = 0
+    t = time.localtime()
+    begin_mon_date = datetime.strptime(('%d-%02d-01' % (t.tm_year, t.tm_mon)), "%Y-%m-%d")
+    try:
+        mon_count = User.query.filter(User.is_admin == False, User.create_time > begin_mon_date).count()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 日新增数
+    day_count = 0
+    t = time.localtime()
+    begin_day_date = datetime.strptime(('%d-%02d-%02d' % (t.tm_year, t.tm_mon, t.tm_mday)), "%Y-%m-%d")
+    try:
+        day_count = User.query.filter(User.is_admin == False, User.create_time > begin_day_date).count()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    data = {
+        "total_count": total_count,
+        "mon_count": mon_count,
+        "day_count": day_count
+    }
+
+    return render_template('admin/user_count.html', data=data)
 
 
 @admin_blue.route('/index')
@@ -33,14 +72,13 @@ def login():
 
     # 查询当前用户
     try:
-        user = User.query.filter(User.mobile==username, User.is_admin==True).first()
+        user = User.query.filter(User.mobile == username, User.is_admin == True).first()
     except Exception as e:
         current_app.logger.error(e)
         return render_template('admin/login.html', errmsg="用户信息查询失败")
 
     if not user:
         return render_template('admin/login.html', errmsg="未查到用户信息")
-
 
     # 校验密码
     if not user.check_passowrd(password):
@@ -51,7 +89,6 @@ def login():
     session["mobile"] = user.mobile
     session["nick_name"] = user.nick_name
     session["is_admin"] = user.is_admin
-
 
     # 跳转到后台管理界面
     return redirect(url_for('admin.index'))
