e5167f0c1580833501459e7bf3f399ca5228f78e
lixiaoyao <liyan__mail@163.com>
2018-10-21 18:15:04 +0800

    修复普通用户能访问管理员页面的bug

--- info/modules/admin/__init__.py
+++ info/modules/admin/__init__.py
@@ -1,6 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, session, redirect, request, url_for
 
 # 创建蓝图对象
 admin_blue = Blueprint("admin", __name__)
 
 from . import views
+
+
+@admin_blue.before_request
+def check_admin():
+    is_admin = session.get("is_admin", False)
+    if not is_admin and not request.url.endswith(url_for('admin.login')):
+        return redirect("/")
